af0690719094fa8c4578b78a4d84f7cf442c60ca
sheikh farid <sf5842843@gmail.com>
2026-06-21 23:45:29 +0000

    Setting up the Google tag (gtag.js) is the key step to start collecting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 
 import { useEffect } from 'react';
 import { initializeFirebase, FirebaseClientProvider } from '@/firebase';
+import Script from 'next/script';
 import './globals.css';
 
 export default function RootLayout({
@@ -21,6 +22,20 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8ETG3NV497"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8ETG3NV497');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
